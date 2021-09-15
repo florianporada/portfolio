00dc6f5ee54731a3070ee6d9daa5ea1fbea99955
Florian Porada <florian.porada@gmail.com>
2021-09-15 15:37:10 +0200

    feat: add dedicated page for projects (WIP)

---
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql, Link } from 'gatsby';
+import TransitionLink from 'gatsby-plugin-transition-link';
+
+export default function WorkTemplate({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data; // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <div className="blog-post-container">
+      <Link to={'/'}>back</Link>
+      Copycopy code to clipboard
+      <TransitionLink>Go to Page 4</TransitionLink>
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </div>
+  );
+}
+
+WorkTemplate.propTypes = {
+  data: PropTypes.object,
+};
+
+export const pageQuery = graphql`
+  query ($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`;
