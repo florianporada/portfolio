e098423f3bcce6b981cb94724f6114bd4e9926c0
Florian Porada <florian.porada@gmail.com>
2021-09-21 11:03:05 +0200

    feat: add transitionstate to pagewrapper component

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -1,15 +1,72 @@
-import styled from 'styled-components';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+import { Spring, animated } from 'react-spring';
+import { TransitionState } from 'gatsby-plugin-transition-link';
 
-const PageWrapper = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  padding: 0 15px 15px 15px;
-  line-height: 1.1;
-  max-width: 992px;
-  margin: 0 auto;
+import SEO from './Seo';
+
+const PageWrapper = styled(animated.div)`
+  ${(props) =>
+    !props.full &&
+    css`
+      position: relative;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      padding: 0 15px 15px 15px;
+      line-height: 1.1;
+      max-width: 992px;
+      margin: 0 auto;
+    `}
 `;
 
-export default PageWrapper;
+const Wrapper = ({ children, full, className }) => {
+  return (
+    <TransitionState>
+      {({ mount, transitionStatus, ...props }) => {
+        return (
+          <>
+            <SEO
+              title={'s'}
+              noscript={[
+                {
+                  innerHTML:
+                    '<meta http-equiv="refresh" content="1;url=simpleversion" />',
+                },
+              ]}
+            />
+            <Spring
+              opacity={
+                ['entering', 'entered'].includes(transitionStatus) ? 1 : 0
+              }
+            >
+              {(styles) => (
+                <PageWrapper
+                  full={full ? 1 : 0}
+                  className={className}
+                  style={styles}
+                >
+                  {children}
+                </PageWrapper>
+              )}
+            </Spring>
+          </>
+        );
+      }}
+    </TransitionState>
+  );
+};
+
+Wrapper.propTypes = {
+  data: PropTypes.object,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  full: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default Wrapper;
